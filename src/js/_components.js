ed0c23b2ff2508b986ebb602cb55f938aab22721
Киленников Роман <kilennikovroman@gmail.com>
2023-07-14 17:08:00 +0300

    add modal

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,5 +1,7 @@
 import './components/accordion';
 import './components/swiper';
+import './components/modal';
+import './components/validation';
 // import './components/smooth-scroll';
 
 
---
+++ src/js/components/validation.js
@@ -0,0 +1,42 @@
+import { validateForms } from '../functions/validate-forms';
+
+
+const rulesModal = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'minLength',
+        value: 2,
+        errorMessage: 'Заполните2 имя!',
+      },
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!',
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+];
+
+
+
+const afterForm = () => {
+  console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.form--modal', rulesModal, afterForm);
+
+
